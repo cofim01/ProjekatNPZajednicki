d580237dc77773a24b3b56a89698daab66e7ad34
filip-mrdak <fm20200181@student.fon.bg.ac.rs>
2024-04-24 18:49:35 +0200

    Dokumentovana klasa Zaduzenje.

--- src/main/java/rs/ac/bg/fon/projekatzajednickimvn/domen/Zaduzenje.java
+++ src/main/java/rs/ac/bg/fon/projekatzajednickimvn/domen/Zaduzenje.java
@@ -8,25 +8,60 @@ import java.util.Date;
 import java.sql.*;
 
 /**
- *
+ * Predstavlja jedno zaduzenje knjige u biblioteci. Svako zaduzenje sadrzi broj zaduzenja, datum zaduzenja, datum razduzenja, primerak knjige koji je zaduzen, clana koji je zaduzio primerak,
+ * korisnika koji je kreirao zaduzenje kao datume za filtriranje zaduzenja.
  * @author MRDAK-PC
  */
 public class Zaduzenje implements OpstiDomenskiObjekat {
-
+    /**
+     * Broj zaduzenja kao int.
+     */
     private int brZaduzenja;
+    /**
+     * Datum zaduzenja (Date).
+     */
     private Date datumZaduzenja;
+    /**
+     * Datum razduzenja (Date).
+     */
     private Date datumRazduzenja;
+    /**
+     * Primerak knjige koji je zaduzen.
+     */
     private PrimerakKnjige primerak;
+    /**
+     * Clan koji je zaduzio primerak knjige.
+     */
     private Clan clan;
+    /**
+     * Korisnik koji je kreirao zaduzenje.
+     */
     private Korisnik korisnik;
+    /**
+     * Početni datum za filtriranje zaduzenja.
+     */
     private Date datumOdFilter;
+    /**
+     * Krajnji datum za filtriranje zaduzenja.
+     */
     private Date datumDoFilter;
-
+    
+    
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-
+    /**
+     * Podrazumevani konstruktor koji kreira novo zaduzenje.
+     */
     public Zaduzenje() {
     }
-
+    /**
+     * Parametrizovani konstruktor koji kreira novo zaduzenje i postavlja vrednosti atributa na unete vrednosti.
+     * @param brZaduzenja - broj novog zaduzenja kao int.
+     * @param datumZaduzenja - datum zaduzenja primerka knjige.
+     * @param datumRazduzenja - datum razduzenja primerka knjige.
+     * @param primerak - primerak knjige koji je zaduzen.
+     * @param clan - clan koji je zaduzio primerak knjige.
+     * @param korisnik - korisnik koji je kreirao zaduzenje.
+     */
     public Zaduzenje(int brZaduzenja, Date datumZaduzenja, Date datumRazduzenja, PrimerakKnjige primerak, Clan clan, Korisnik korisnik) {
         this.brZaduzenja = brZaduzenja;
         this.datumZaduzenja = datumZaduzenja;
@@ -37,62 +72,102 @@ public class Zaduzenje implements OpstiDomenskiObjekat {
     }
 
     
-
+    /**
+     * Postavlja broj zaduzenja na unetu vrednost.
+     * @param brZaduzenja - broj zaduzenja kao int.
+     */
     public void setBrZaduzenja(int brZaduzenja) {
         this.brZaduzenja = brZaduzenja;
     }
-
+    /**
+     * Postavlja datum zaduzenja primerka knjige na prosledjeni datum.
+     * @param datumZaduzenja - datum zaduzenja primerka knjige.
+     */
     public void setDatumZaduzenja(Date datumZaduzenja) {
         this.datumZaduzenja = datumZaduzenja;
     }
-
+    /**
+     * Postavlja datum razduzenja primerka knjige na prosledjeni datum.
+     * @param datumRazduzenja - datum razduzenja primerka knjige.
+     */
     public void setDatumRazduzenja(Date datumRazduzenja) {
         this.datumRazduzenja = datumRazduzenja;
     }
-
+    /**
+     * Postavlja clana koji je zaduzio primerak knjige.
+     * @param clan - clan koji je zaduzio primerak knjige.
+     */
     public void setClan(Clan clan) {
         this.clan = clan;
     }
-
+    /**
+     * Postavlja korisnika koji je kreirao zaduzenje.
+     * @param korisnik - korisnik koji je kreirao zaduzenje.
+     */
     public void setKorisnik(Korisnik korisnik) {
         this.korisnik = korisnik;
     }
-
+    /**
+     * Postavlja primerak knjige koji je zaduzen.
+     * @param primerak - primerak knjige koji je zaduzen.
+     */
     public void setPrimerak(PrimerakKnjige primerak) {
         this.primerak = primerak;
     }
-
+    /**
+     * Postavlja pocetni datum za filtriranje zaduzenja.
+     * @param datumDoFilter - pocetni datum za filtriranje zaduzenja.
+     */
     public void setDatumDoFilter(Date datumDoFilter) {
         this.datumDoFilter = datumDoFilter;
     }
-
+    /**
+     * Postavlja krajnji datum za filtriranje zaduzenja.
+     * @param datumOdFilter - krajnji datum za filtriranje zaduzenja.
+     */
     public void setDatumOdFilter(Date datumOdFilter) {
         this.datumOdFilter = datumOdFilter;
     }
-    
-    
-
+    /**
+     * Vraca broj zaduzenja primerka knjige.
+     * @return broj zaduzenja kao int.
+     */
     public int getBrZaduzenja() {
         return brZaduzenja;
     }
-
+    /**
+     * Vraca primerak knjige koji je zaduzen.
+     * @return primerak knjige koji je zaduzen.
+     */
     public PrimerakKnjige getPrimerak() {
         return primerak;
     }
     
-
+    /**
+     * Vraca datum zaduzenja primerka knjige.
+     * @return datum zaduzenja primerka knjige.
+     */
     public Date getDatumZaduzenja() {
         return datumZaduzenja;
     }
-
+    /**
+     * Vraca datum razduzenja primerka knjige.
+     * @return datum razduzenja primerka knjige.
+     */
     public Date getDatumRazduzenja() {
         return datumRazduzenja;
     }
-
+    /**
+     * Vraca clana koji je zaduzio primerak knjige.
+     * @return clan koji je zaduzio primerak knjige.
+     */
     public Clan getClan() {
         return clan;
     }
-
+    /**
+     * Vraca korisnika koji je kreirao zaduzenje.
+     * @return korisnik koji je kreirao zaduzenje.
+     */
     public Korisnik getKorisnik() {
         return korisnik;
     }
