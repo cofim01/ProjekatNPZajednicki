8ed9839b9e51271d5659b678eb3f720397d409ac
filip-mrdak <fm20200181@student.fon.bg.ac.rs>
2024-04-24 19:33:56 +0200

    Dokumentovana enumeracija Operacije.

--- src/main/java/rs/ac/bg/fon/projekatzajednickimvn/komunikacija/Operacije.java
+++ src/main/java/rs/ac/bg/fon/projekatzajednickimvn/komunikacija/Operacije.java
@@ -3,29 +3,32 @@ package rs.ac.bg.fon.projekatzajednickimvn.komunikacija;
 import java.io.Serializable;
 
 /**
- *
- * @author MRDAK-PC
+ * Enumeracija koja sadrži moguće operacije koje se mogu izvršiti u sistemu biblioteke.
+ * Ove operacije se koriste za komunikaciju između korisničkog interfejsa i serverske logike.
+ * Svaka operacija ima svoju jedinstvenu vrednost koja se koristi za identifikaciju.
+ * 
+ * @author Filip Mrdak
  */
 public enum Operacije implements Serializable{
-    LOGOVANJE,
-    DODAJ_KNJIGU,
-    VRATI_SVE_KNJIGE,
-    OBRSI_KNJIGU,
-    DODAJ_CLANA,
-    AZURIRAJ_CLANA,
-    VRATI_SVE_CLANOVE,
-    VRATI_SVE_ZANROVE,
-    VRATI_SVE_IZDAVACE,
-    VRATI_SVE_AUTORE,
-    DODAJ_ZADUZENJE,
-    RAZDUZI,
-    DODAJ_AUTORE_KNJIGE,
-    VRATI_SVE_AUTORE_KNJIGE,
-    VRATI_SVA_ZADUZENJA,
-    VRATI_SVE_PRIMERKE,
-    DODAJ_PRIMERAK,
-    IZMENI_PRIMERAK,
-    VRATI_CLANOVE_SA_USLOVOM,
-    VRATI_KNJIGE_SA_USLOVOM,
-    ODJAVA;
+    LOGOVANJE,                  // Operacija za prijavljivanje korisnika
+    DODAJ_KNJIGU,               // Operacija za dodavanje nove knjige u sistem
+    VRATI_SVE_KNJIGE,           // Operacija za dobijanje svih knjiga iz sistema
+    OBRSI_KNJIGU,               // Operacija za brisanje knjige iz sistema
+    DODAJ_CLANA,                // Operacija za dodavanje novog člana u sistem
+    AZURIRAJ_CLANA,             // Operacija za ažuriranje podataka o članu
+    VRATI_SVE_CLANOVE,          // Operacija za dobijanje svih članova iz sistema
+    VRATI_SVE_ZANROVE,          // Operacija za dobijanje svih žanrova knjiga iz sistema
+    VRATI_SVE_IZDAVACE,         // Operacija za dobijanje svih izdavača knjiga iz sistema
+    VRATI_SVE_AUTORE,           // Operacija za dobijanje svih autora knjiga iz sistema
+    DODAJ_ZADUZENJE,            // Operacija za dodavanje novog zaduženja (iznajmljivanje knjige)
+    RAZDUZI,                    // Operacija za razduživanje (vraćanje) knjige
+    DODAJ_AUTORE_KNJIGE,        // Operacija za dodavanje autora knjige
+    VRATI_SVE_AUTORE_KNJIGE,    // Operacija za dobijanje svih autora knjige iz sistema
+    VRATI_SVA_ZADUZENJA,        // Operacija za dobijanje svih zaduženja (iznajmljivanja) iz sistema
+    VRATI_SVE_PRIMERKE,         // Operacija za dobijanje svih primeraka knjiga iz sistema
+    DODAJ_PRIMERAK,             // Operacija za dodavanje novog primerka knjige u sistem
+    IZMENI_PRIMERAK,            // Operacija za izmenu podataka o primerku knjige
+    VRATI_CLANOVE_SA_USLOVOM,   // Operacija za pretragu članova po određenom uslovu
+    VRATI_KNJIGE_SA_USLOVOM,    // Operacija za pretragu knjiga po određenom uslovu
+    ODJAVA;                     // Operacija za odjavljivanje korisnika iz sistema
 }
