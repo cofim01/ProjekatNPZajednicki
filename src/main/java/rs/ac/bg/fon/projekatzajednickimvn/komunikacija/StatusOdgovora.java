1f62ce6005e499df9a00cb8d757857e5e5fbb711
filip-mrdak <fm20200181@student.fon.bg.ac.rs>
2024-04-24 19:40:56 +0200

    Dokumentovana enumeracija StatusOdgovora

--- src/main/java/rs/ac/bg/fon/projekatzajednickimvn/komunikacija/StatusOdgovora.java
+++ src/main/java/rs/ac/bg/fon/projekatzajednickimvn/komunikacija/StatusOdgovora.java
@@ -4,9 +4,12 @@ package rs.ac.bg.fon.projekatzajednickimvn.komunikacija;
 import java.io.Serializable;
 
 /**
- *
- * @author MRDAK-PC
+ * Enumeracija koja definise moguce statuse odgovora koji se koriste u aplikaciji.
+ * Ovi statusi se koriste za oznacavanje rezultata operacija u sistemu.
+ * 
+ * @author Filip Mrdak
  */
 public enum StatusOdgovora implements Serializable{
-    SUCCESS,ERRROR;
+    SUCCESS,    // Oznacava uspesan rezultat operacije u sistemu.
+    ERROR;      // Oznacava gresku ili neuspesan rezultat operacije u sistemu.
 }
